7bc5f09a5d39aa769c97c690bded34453055fc31
Viral Vaghela <viralvaghela8577@gmail.com>
2026-06-26 20:09:15 +0530

    [Add CORs from .env]

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,19 @@ const cors = require("cors");
 
 const app = express();
 
+let allowedOrigins = [process.env.CORS_ORIGIN];
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
-  method: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
 
 app.use(cors(corsOptions));
