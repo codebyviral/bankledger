22d071fd8f6591c5de77b3d24a4186b9367798c1
Viral Vaghela <viralvaghela8577@gmail.com>
2026-06-26 20:19:49 +0530

    [Remove] timeout from api client

--- frontend/src/api/api-client.ts
+++ frontend/src/api/api-client.ts
@@ -2,12 +2,10 @@ import axios from "axios";
 
 const publicInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 5000,
 });
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 5000,
   withCredentials: true,
 });
 
