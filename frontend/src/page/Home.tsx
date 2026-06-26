18ea07a8d66af86d581f95e1c6d86419e8d09bfb
Viral Vaghela <viralvaghela8577@gmail.com>
2026-06-26 20:17:47 +0530

    [Rename] redirect routes

--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -11,8 +11,8 @@ const Home = () => {
       <div className='h-screen flex justify-center items-center flex-col'>
         <h1 className='text-3xl'>Welcome to Bank Ledger</h1>
         <div className='mt-3 flex flex-row gap-3'>
-          <Button onClick={() => navigate('/login')} className='text-lg'>Login</Button>
-          <Button onClick={() => navigate('/signup')} className='text-lg'>Signup</Button>
+          <Button onClick={() => navigate('/auth/signin')} className='text-lg'>Login</Button>
+          <Button onClick={() => navigate('/auth/signup')} className='text-lg'>Signup</Button>
           <Button className='h-auto' variant='ghost' onClick={() => {
             setTheme(theme === 'dark' ? 'light' : 'dark')
           }}><Moon /></Button>
